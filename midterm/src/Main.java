b485ef42cb0480b502b64c47f7613bf7ce8ae4bd
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-03-27 21:11:48 +0800

    fixed error

--- midterm/src/Main.java
+++ midterm/src/Main.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Menu: \n1 -- Railfence\n2 -- Vigenere");
-        System.out.print("Input:");
-        int input = scan.nextInt();
 
-        switch (input) {
-            case 1:
-                RailFence railFence = new RailFence();
-                RailFence.main(args);
-                main(args);
-                break;
+        while (true) {
+            System.out.println("Menu: \n1 -- Railfence\n2 -- Vigenere\n3 -- Exit");
+            System.out.print("Input: ");
 
-            case 2:
-                Vigenere vigenere = new Vigenere();
-                vigenere.main(args);
-                main(args);
-                break;
-            default:
-                return;
+            if (scan.hasNextInt()) {
+                int input = scan.nextInt();
+
+                switch (input) {
+                    case 1:
+                        RailFence railFence = new RailFence();
+                        railFence.main(args);
+                        break;
+                    case 2:
+                        Vigenere vigenere = new Vigenere();
+                        vigenere.main(args);
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        System.out.println("Invalid input. Please try again.");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("Invalid input. Please enter a number (1-3).");
+            }
         }
     }
-
 }
--- midterm/src/RailFence.java
+++ midterm/src/RailFence.java
@@ -2,20 +2,53 @@ import java.util.Scanner;
 
 public class RailFence {
     public static void main(String[] args) {
-        try (Scanner scan = new Scanner(System.in)) {
-            System.out.print("Enter Text: ");
-            String plaintext = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        Menu(scan);
+    }
 
-            System.out.print("Enter key: ");
-            int key = scan.nextInt();
+    public static void Menu(Scanner scan) {
+        System.out.println("-- RailFence Cipher -- \n1 -- Encrypt\n2 -- Decrypt\n3 -- Exit");
+        System.out.print("Input: ");
+        int input = scan.nextInt();
 
-            String encryptMsg = encryptProcess(plaintext, key);
-            System.out.print("Encrpted Message: " + encryptMsg);
-            System.out.println("");
-
-            String decryptMsg = decryptProcess(encryptMsg, key);
-            System.out.print("Decrypted Message: " + decryptMsg);
+        switch (input) {
+            case 1:
+                Encrypt(scan);
+                Menu(scan);
+                break;
+            case 2:
+                Decrypt(scan);
+                Menu(scan);
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Error, Please input the correct number");
+                Menu(scan);
+                break;
         }
+    }
+
+    public static void Encrypt(Scanner scan) {
+        System.out.print("Enter Text to Encrypt: ");
+        String plaintext = scan.next();
+
+        System.out.print("Enter Key: ");
+        int key = scan.nextInt();
+
+        String encryptedMessage = encryptProcess(plaintext, key);
+        System.out.println("Encrypted Message: " + encryptedMessage);
+    }
+
+    public static void Decrypt(Scanner scan) {
+        System.out.print("Enter Text to Decrypt: ");
+        String encryptedText = scan.next();
+
+        System.out.print("Enter Key: ");
+        int key = scan.nextInt();
+
+        String decryptedMessage = decryptProcess(encryptedText, key);
+        System.out.println("Decrypted Message: " + decryptedMessage);
     }
 
     static String encryptProcess(String text, int key) {
