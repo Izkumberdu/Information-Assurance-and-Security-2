e850f630b37f76145562d1c7bc442c403daef8c3
Mariane Historia <marianerosario.historia.21@usjr.edu.ph>
2024-03-28 12:16:24 +0800

    menu

--- midterm/src/Main.java
+++ midterm/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Menu: \n1 -- Railfence\n2 -- Vigenere\n3 -- Exit");
+            System.out.println("Menu: \n1 -- Railfence\n2 -- Vigenere\n3 -- DES\n4 -- Exit");
             System.out.print("Input: ");
 
             if (scan.hasNextInt()) {
@@ -20,7 +20,12 @@ public class Main {
                         Vigenere vigenere = new Vigenere();
                         vigenere.main(args);
                         break;
+
                     case 3:
+                        DES des = new DES();
+                        des.main(args);
+                        break;
+                    case 4:
                         return;
                     default:
                         System.out.println("Invalid input. Please try again.");
