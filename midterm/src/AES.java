f4059997f7e6c6ca5927e4404935e8168bf4ea4e
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-03-31 15:57:17 +0800

    added menu

--- midterm/src/AES.java
+++ midterm/src/AES.java
@@ -1,3 +1,34 @@
-public class AES {
+import java.util.Scanner;
 
+public class AES {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        menu(scan);
+
+    }
+
+    public static void menu(Scanner scan) {
+        System.out.println("-- Advanced Encryption Standard -- \n1 -- Encrypt\n2 -- Decrypt\n3 -- Exit");
+        System.out.print("Input: ");
+        int input = scan.nextInt();
+
+        switch (input) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+
+            default:
+                System.out.println("Error, Please input the correct number");
+                break;
+        }
+    }
+
+    public static void Encrypt() {
+
+    }
 }
