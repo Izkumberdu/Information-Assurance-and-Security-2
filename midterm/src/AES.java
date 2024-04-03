dcf06b261ad3f2c5a7890eae3caccabf644777df
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-04-03 17:59:57 +0800

    test

--- midterm/src/AES.java
+++ midterm/src/AES.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class AES {
+    private static final int BLOCK_SIZE = 16;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         menu(scan);
-
     }
 
     public static void menu(Scanner scan) {
@@ -14,30 +15,47 @@ public class AES {
 
         switch (input) {
             case 1:
-                Encrypt(scan);
-                menu(scan);
+                encryptMenu(scan);
                 break;
             case 2:
-                Decrypt(scan);
-                menu(scan);
+                decryptMenu(scan);
                 break;
             case 3:
-
+                System.out.println("Exiting...");
                 break;
-
             default:
                 System.out.println("Error, Please input the correct number");
                 break;
         }
     }
 
-    public static void Encrypt(Scanner scan) {
+    public static void encryptMenu(Scanner scan) {
         System.out.println("Enter Text To encrypt:");
         String text = scan.next();
-
+        System.out.println("Enter Encryption Key (16 bytes in hexadecimal format):");
+        String key = scan.next();
+        String encryptedText = encryptECB(text, key);
+        System.out.println("Encrypted Text: " + encryptedText);
+        menu(scan);
     }
 
-    public static void Decrypt(Scanner scan) {
+    public static void decryptMenu(Scanner scan) {
+        System.out.println("Enter Text To decrypt:");
+        String text = scan.next();
+        System.out.println("Enter Decryption Key (16 bytes in hexadecimal format):");
+        String key = scan.next();
+        String decryptedText = decryptECB(text, key);
+        System.out.println("Decrypted Text: " + decryptedText);
+        menu(scan);
+    }
 
+    private static String encryptECB(String input, String key) {
+
+        return input;
+    }
+
+    private static String decryptECB(String input, String key) {
+
+        return input;
     }
 }
